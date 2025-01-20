133caf4cbab56efcea89d4629b6ac9368c960ae4
Bizimungu Pascal <bizip04@gmail.com>
2025-01-20 09:43:33 +0200

    Fix redirection issue on sign up

--- src/component/Signup.js
+++ src/component/Signup.js
@@ -11,15 +11,19 @@ const Signup = () => {
     password: '',
     remember: false
   });
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
         await API.post('/auth/signup', formData);
-  
+        window.location.href = '/login';
     } catch (error) {
       console.error('Error:', error);
       // Handle error
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -140,8 +144,9 @@ const Signup = () => {
               <button
                 type="submit"
                 className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full bg-blue-700 hover:bg-blue-800 "
+                disabled={loading}
               >
-                Sign up
+                {loading ? 'Loading...' : 'Sign up'}
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Already have an account?{' '}
